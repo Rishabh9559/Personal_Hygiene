7d1a4a566ca2dd82326f36999d129d3b248c3685
Rishabh Kushwaha <Rishabhkushwaha9559@gmail.com>
2024-11-08 18:33:48 +0530

    update tipes

--- quiz.js
+++ quiz.js
@@ -150,7 +150,7 @@ function loadQuestion(index) {
 }
 
 next.addEventListener("click", (e) => {
-  e.preventDefault(); // Prevent form submission behavior
+  e.preventDefault(); 
 
   let selected = document.querySelector('input[name="question"]:checked');
   if (selected) {
@@ -240,3 +240,5 @@ Tipspost.addEventListener("click", (e) => {
     window.location.reload();
   });
 });
+
+
